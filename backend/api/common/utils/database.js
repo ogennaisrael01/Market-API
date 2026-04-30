841f21bb31a8e5a8d49180ca82d70f1c9e0ffa8b
ogennaisrael01 <ogennaisrael98@gmail.com>
2026-04-30 23:33:33 +0100

    chore: clean up database configuration and improve readability

--- backend/api/common/utils/database.js
+++ backend/api/common/utils/database.js
@@ -11,11 +11,12 @@ Sequelize.useCLS(nameSpace)
 const postgresDatabase = process.env.POSTGRES_DATABASE
 const postgresUsername = process.env.POSTGRES_USERNAME
 const postgresPassword = process.env.POSTGRES_PASSWORD
+const postgresHost = process.env.POSTGRES_HOST
 
 
 export const sequelize = new Sequelize( postgresDatabase, postgresUsername, postgresPassword, {
     "dialect": "postgres",
     // "storage": "./storage/market-place.db"
-    "host": "localhost",
+    "host": postgresHost,
     'logging': false
 });
